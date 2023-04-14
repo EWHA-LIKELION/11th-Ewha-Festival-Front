d8e61b4378b7d56fcd87db975dced74f5c660b1d
gchaewon <conjs1@naver.com>
2023-04-14 15:38:10 +0900

    "feat: 페이지 아이콘 변경 추가"

--- src/components/_common/topBar/TopBar.jsx
+++ src/components/_common/topBar/TopBar.jsx
@@ -4,16 +4,25 @@ import { useNavigate } from "react-router-dom";
 import * as S from "./TopBar.style"
 // icons
 import { HiMenu, HiOutlineSearch} from "react-icons/hi";
+import { BsFillPersonFill } from "react-icons/bs";
 
 const TopBar = props =>{
     const navigate = useNavigate();
+    const path = window.location.pathname;
     return(<>
         <S.Wrapper>
             <HiMenu/>
-            <S.Title>
-                {props.title}
-            </S.Title>
-            <HiOutlineSearch onClick={()=>{navigate("/search")}}/>
+            { path==="/"? 
+                <S.Title style={{visibility: "hidden"}}>{props.title}</S.Title>
+                :<S.Title>{props.title}</S.Title>
+            }
+            { path==="/"?
+                <BsFillPersonFill/>
+                :path.includes("booth")?
+                    <HiOutlineSearch onClick={()=>{navigate("/search")}}/>
+                    : <HiOutlineSearch style={{visibility:"hidden"}}/>
+            }           
+            
         </S.Wrapper>
     </>)
 }
