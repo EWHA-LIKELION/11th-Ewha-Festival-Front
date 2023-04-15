103d1bf5928a652a922cb91beace7d72902ed95e
gchaewon <conjs1@naver.com>
2023-04-15 15:22:21 +0900

    feat: 페이지에 따른 컨텐츠 변경 추가

--- src/components/_common/modal/Modal.style.js
+++ src/components/_common/modal/Modal.style.js
@@ -23,6 +23,7 @@ export const Container = styled.div`
 
     display: flex;
     justify-content: center;
+    font-family: var(--pre-font);
 `
 export const ModalWrapper = styled.div`
     width: 80%;
@@ -40,6 +41,19 @@ export const ModalWrapper = styled.div`
         margin-top: 0;
         }
     }
+    border-radius: 8px;
+    background-color: #ffffff;
+    svg{
+        width: 62px;
+        height: 42px;
+        color: var(--red);
+    }
+`
+export const ModalContainer = styled.div`
+    height: 164px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 `
 export const ModalTitle = styled.div`
     height: 49px;
@@ -54,15 +68,6 @@ export const ModalTitle = styled.div`
     color: #ffffff;
     font-weight: 700;
 `
-export const ModalContainer = styled.div`
-    height: 164px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-
-    background-color: #ffffff;
-    border-radius: 0 0 8px 8px;
-`
 export const ModalSubtitle = styled.div`
     margin: 21px 0 16px 0;
     font-size: 12px;
@@ -74,15 +79,48 @@ export const ModalContents = styled.div`
 
     text-align: center;
     font-size: 14px;
-    word-break: keep-all;
     color: #004628;
+    white-space: pre-line;
 `
-export const DeleteBtn = styled.button`
-    width: 96px;
-    height: 30px;
-
-    background-color: #029C54;
-    color: #ffffff;
-    border-radius: 8px;
+export const AlertWrapper = styled.div`
+    width: 100%;
+    margin-top: 24px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+export const AlertText = styled.div`
+    margin-top: 9px;
+    color: var(--green1);
+    font-weight: 400;
+    font-size: 11.5px;
+    line-height: 18px;
+`
+export const AlertMessage = styled.div`
+    margin: 9px 0 17px 0;
+    color: var(--red);
+    font-weight: 500;
+    font-size: 13px;
+`
+export const ButtonWrapper = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    button{
+        width: 96px;
+        height: 30px;
+        border-radius: 8px;
+        box-shadow: 0px 2px 6px rgba(165, 165, 165, 0.2);
+    }
+`
+export const WhiteBtn = styled.button`
+    background-color: var(--white);
+    margin-right: 10px;
+    color: var(--green2);
+    border: 0.4px solid var(--green2);
+`
+export const GreenBtn = styled.button`
+    background-color: var(--green2);
+    color: var(--white);
     border: none;
 `
