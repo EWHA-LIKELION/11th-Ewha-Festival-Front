ee89cb8457a278a991c6637a1aa7710599d27522
서진 <dltjwls0529@gmail.com>
2023-04-28 15:46:25 +0900

    fix: useBookmark 오류 수정, 로그인 상태 검사 추가

--- src/components/_common/useBookmark.js
+++ src/components/_common/useBookmark.js
@@ -1,46 +1,52 @@
 import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const useBookmark = (isBookmarked, currentBoothID) => {
+  const nav = useNavigate();
   const [state, setState] = useState(isBookmarked);
-  const [trigger, setTrigger] = useState(0);
-  let id = Number(currentBoothID);
-  const toggle = () => {
-    // toggle 함수 실행 시 trigger 값이 변경됨
-    setTrigger(trigger + 1);
-  };
+  const [trigger, setTrigger] = useState(Date.now());
+  const [id, setId] = useState(0);
+  useEffect(() => {
+    setState(isBookmarked);
+    setId(Number(currentBoothID));
+  }, [isBookmarked]);
+  const [isLogin, setIsLogin] = useState(null);
+  const token = localStorage.getItem('token');
+  useEffect(() => {
+    if (token === null) {
+      //setIsLogin(false);
+    }
+    if (token !== null) {
+      //setIsLogin(true);
+    }
+  }, []);
   useEffect(() => {
     if (typeof isBookmarked !== 'boolean') return;
-    // 1번째 클릭 undefined로 씹히는 현상 해결해야함
-    //console.log(state);
-    if (state) {
-      // if (login) {
-      //   Unlike(id)
-      //     .then(res => {
-      //       console.log(res);
-      //       setState(!state);
-      //     })
-      //     .catch(err => console.log(err));
-      // } else {
-      //   alert('로그인 후 북마크 기능을 사용할 수 있습니다.');
-      //   nav(to login);
-      // }
+    if (isLogin) {
+      if (state) {
+        // Unlike(id)
+        //   .then(res => {
+        //     console.log(res);
+        //     setState(!state);
+        //     alert('북마크 해제 완료되었습니다.');
+        //   })
+        //   .catch(err => console.log(err));
+      } else {
+        // Like(id)
+        // .then(res => {
+        //   console.log(res);
+        //   setState(!state);
+        //   alert('북마크 설정 완료되었습니다.');
+        // })
+        // .catch(err => console.log(err));
+      }
       setState(!state);
     } else {
-      // if (login) {
-      //   Like(id)
-      //     .then(res => {
-      //       console.log(res);
-      //       setState(!state);
-      //     })
-      //     .catch(err => console.log(err));
-      // } else {
-      //   alert('로그인 후 북마크 기능을 사용할 수 있습니다.');
-      //   nav(to login);
-      // }
-      setState(!state);
+      alert('로그인 후 북마크 기능을 사용할 수 있습니다.');
+      nav('/auth/login');
     }
   }, [trigger]);
-  // deps에 trigger를 추가하여 refetch 될 때마다 내부 코드가 실행되도록 함
+  const toggle = () => setTrigger(trigger + 1);
   return { state, toggle };
 };
 
