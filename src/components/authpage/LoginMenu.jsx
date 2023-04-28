b22acbd68fe2be93d1f6e33d59cf99a6458c2319
yyeonzu <tooma22@naver.com>
2023-04-28 15:51:36 +0900

    chore: topbar 경로 변경

--- src/components/authpage/LoginMenu.jsx
+++ src/components/authpage/LoginMenu.jsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 import * as S from './LoginReigster.style';
 import { BiUser, BiLockOpen } from 'react-icons/bi';
 // components
-import TopBar from '../_common/topbar/TopBar';
+import TopBar from '../_common/topBar/TopBar';
 
 const LoginMenu = () => {
   const navigate = useNavigate();
