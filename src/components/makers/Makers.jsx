d42828bb3e492e593a05e94e0961bffa792d4eeb
서진 <dltjwls0529@gmail.com>
2023-05-01 16:52:58 +0900

    feat: 만든이들 페이지 퍼블리싱

---
+++ src/components/makers/Makers.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import TopBar from '../_common/topbar/TopBar';
+import Footer from '../_common/footer/Footer';
+import LikeLionMember from './LikeLionMember';
+import TFMember from './TFMember';
+
+import { makerslist } from './makersdata';
+import { COM } from './Makers.style';
+import titlebackground from '../../assets/images/makerspage/titlebackground.svg';
+import titlestar from '../../assets/images/boothdetailpage/titlestar.svg';
+
+const Makers = () => {
+  return (
+    <COM.Wrapper>
+      <TopBar title='만든이들' />
+      <COM.Container>
+        <COM.Title>
+          <object type='image/svg+xml' data={titlebackground}>
+            <COM.TitleImg src={titlebackground} />
+          </object>
+          <div className='text'>
+            이화여대 멋쟁이사자처럼 &nbsp;&nbsp; × &nbsp;&nbsp;대동제 TF
+          </div>
+        </COM.Title>
+        <COM.SubTitle>
+          <img src={titlestar} />
+          <div className='text'>이화여대 멋쟁이사자처럼 11기 운영진</div>
+          <div className='border' />
+        </COM.SubTitle>
+        {makerslist.likelion.map(team => {
+          return <LikeLionMember team={team} />;
+        })}
+        <div style={{ height: '50px' }} />
+        <COM.SubTitle>
+          <img src={titlestar} />
+          <div className='text'>대동제 TF</div>
+          <div className='border' />
+        </COM.SubTitle>
+        {makerslist.tf.map(team => {
+          return <TFMember team={team} />;
+        })}
+      </COM.Container>
+      <Footer />
+    </COM.Wrapper>
+  );
+};
+
+export default Makers;
