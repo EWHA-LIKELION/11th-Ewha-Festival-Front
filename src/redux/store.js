93bea6152afc64b475be9eca1b91100df17c5d2e
gchaewon <conjs1@naver.com>
2023-05-01 21:12:01 +0900

    chore: pageSlice로 이름 변경

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,12 +8,12 @@ import { persistReducer } from 'redux-persist';
 import thunk from 'redux-thunk';
 import userReducer from './userSlice';
 import boothReducer from './boothSlice';
-import mypageReducer from './mypageSlice';
+import mypageReducer from './pageSlice';
 
 const reducers = combineReducers({
   user: userReducer,
   booth: boothReducer,
-  mypage: mypageReducer,
+  page: mypageReducer,
 });
 
 const persistConfig = {
