928a28116ad0e46e24f06c507d9d97c78cfb0436
yyeonzu <tooma22@naver.com>
2023-05-02 06:04:19 +0900

    feat: 부스 카테고리 UI 퍼블리싱 및 일부 api 연결

--- src/components/boothpage/Booth.style.js
+++ src/components/boothpage/Booth.style.js
@@ -1,20 +1,26 @@
 import styled from 'styled-components';
+// image
+import daychecked from '../../assets/images/boothpage/daychecked.png';
+import viewerchecked from '../../assets/images/boothpage/viewerchecked.svg';
 
 // booth component
-
 const ComponentContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   padding: 4px;
   height: 60vmin;
-  /* border: solid; */
+
+  .closed {
+    filter: grayscale(70%);
+    color: var(--gray2);
+  }
 `;
 
 const ImageWrapper = styled.div`
   position: relative;
   border: solid 1px black;
-  border-radius: 4px;
+  border-radius: 8px;
 
   width: 43vmin;
   height: 43vmin;
@@ -26,6 +32,19 @@ const ImageWrapper = styled.div`
     top: 0;
     left: 0;
     object-fit: cover;
+  }
+
+  .closed {
+    position: absolute;
+    z-index: 1;
+    font-size: 20px;
+    font-weight: 600;
+    color: var(--white);
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
 `;
 
@@ -39,6 +58,10 @@ const LocationContainer = styled.div`
   font-size: 12px;
   color: var(--red);
   margin: 4px 0 4px 0;
+
+  .close {
+    color: var(--gray2);
+  }
 `;
 
 const TitleWrapper = styled.div`
@@ -76,36 +99,126 @@ export const C = {
   Hashtag,
 };
 
+// filterbar
+
+const DayFilterContainer = styled.div`
+  width: 100%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  border-bottom: solid 1px var(--gray3);
+  overflow: hidden;
+`;
+
+const DayContainer = styled.div`
+  color: ${props => (props.checked ? 'var(--green2)' : 'var(--gray2)')};
+  background-image: ${props => (props.checked ? `url(${daychecked})` : 'none')};
+  background-size: 102%;
+  border-bottom: ${props =>
+    props.checked ? 'solid 3px var(--green2)' : 'none'};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 60px;
+  height: 50px;
+
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+
+  .number {
+    font-size: 13px;
+    font-weight: 300;
+    line-height: 15px;
+  }
+  .day {
+    font-weight: ${props => (props.checked ? '700' : '500')};
+    font-size: 16px;
+    line-height: 20px;
+  }
+`;
+
+const ViewerFilterContainer = styled.div`
+  width: 80%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+`;
+
+const ViewerContainer = styled.div`
+  color: ${props => (props.checked ? 'var(--white)' : 'var(--gray2)')};
+  background-image: ${props =>
+    props.checked ? `url(${viewerchecked})` : 'none'};
+  /* background-size: 100%; */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 105px;
+  height: 30px;
+  font-weight: ${props => (props.checked ? '600' : '400')};
+  font-size: 13px;
+  line-height: 13px;
+
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+`;
+
+const DataGrid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+`;
+const DataContainer = styled.div`
+  width: 72px;
+  height: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${props =>
+    props.checked ? 'var(--green2)' : 'var(--white)'};
+  color: ${props => (!props.checked ? 'var(--green2)' : 'var(--white)')};
+  border: solid 1px var(--green2);
+  border-radius: 104px;
+  font-size: 14px;
+  margin: 3px 4px 3px 4px;
+`;
+
+export const F = {
+  DayFilterContainer,
+  DayContainer,
+  ViewerFilterContainer,
+  ViewerContainer,
+  DataGrid,
+  DataContainer,
+};
+
 // boothpage
 
 const Wrapper = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: center;
   background-color: var(--beige);
   overflow: auto;
 `;
-
-const DayContainer = styled.div`
-  display: flex;
-  border: solid 1px;
-`;
-
-const ViewerContainer = styled.div`
-  display: flex;
-`;
-
-const UnderViewerContainer = styled.div``;
 
 const ComponentGrid = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
 `;
 
+const BoothLength = styled.div`
+  width: 85%;
+  font-weight: 500;
+  font-size: 13px;
+  line-height: 15px;
+  color: var(--gray2);
+  margin: 8px 0 4px 0;
+`;
+
 export const B = {
   Wrapper,
-  DayContainer,
-  ViewerContainer,
-  UnderViewerContainer,
   ComponentGrid,
+  BoothLength,
 };
--- src/components/boothpage/BoothComponent.jsx
+++ src/components/boothpage/BoothComponent.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 // api
 
@@ -8,32 +9,53 @@ import { C } from './Booth.style';
 // image
 import fillheart from '../../assets/images/fillheart.svg';
 import strokeheart from '../../assets/images/strokeheart.svg';
-import { useNavigate } from 'react-router-dom';
+import { useAppSelector } from '../../redux/store';
+import { LikeBooth } from '../../api/booth';
 
 const BoothComponent = props => {
-  const { id, college, number, category, name, hashtag, is_liked, thumnail } =
-    props;
+  const {
+    id,
+    college,
+    number,
+    category,
+    name,
+    hashtag,
+    is_liked,
+    thumnail,
+    opened,
+  } = props;
 
+  const booth_id = useAppSelector(state => state.user);
   const navigate = useNavigate();
 
   const [isLiked, setIsLiked] = useState(is_liked);
 
   const changeLiked = () => {
-    // 서버에 제출 & UI 변경
-    setIsLiked(!isLiked);
+    // 서버에 제출
+    const token = localStorage.getItem('token');
+    if (token) {
+      setIsLiked(!isLiked);
+    } else {
+      alert('로그인이 필요한 서비스입니다');
+    }
   };
   return (
     <>
       <C.ComponentContainer>
         <C.ImageWrapper onClick={() => navigate(`/booth/detail/:${id}`)}>
-          {thumnail ? <img src={thumnail} /> : <div>썸네일 없음</div>}
+          {thumnail ? (
+            <img className={opened ? 'open' : 'closed'} src={thumnail} />
+          ) : (
+            ''
+          )}
+          {opened ? '' : <div className='closed'>운영 종료</div>}
         </C.ImageWrapper>
         <C.LocationContainer>
-          <div>
+          <div className={opened ? 'open' : 'closed'}>
             {college}
             {number}•{category}
           </div>
-          <div onClick={() => changeLiked()}>
+          <div onClick={() => changeLiked(id)}>
             {isLiked ? (
               <img src={fillheart} width='18px' />
             ) : (
@@ -42,8 +64,15 @@ const BoothComponent = props => {
           </div>
         </C.LocationContainer>
         <C.TitleWrapper>
-          <C.BoothTitle length={name.length}>{name}</C.BoothTitle>
-          <C.Hashtag>{hashtag}</C.Hashtag>
+          <C.BoothTitle
+            className={opened ? 'open' : 'closed'}
+            length={name.length}
+          >
+            {name}
+          </C.BoothTitle>
+          <C.Hashtag className={opened ? 'open' : 'closed'}>
+            {hashtag}
+          </C.Hashtag>
         </C.TitleWrapper>
       </C.ComponentContainer>
     </>
