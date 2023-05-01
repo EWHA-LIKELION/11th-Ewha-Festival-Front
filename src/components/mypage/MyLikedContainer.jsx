d1ad1de1ebefe6060819f349a9c36828156bbac1
gchaewon <conjs1@naver.com>
2023-05-01 21:13:12 +0900

    chore: 불필요한 코드 삭제

--- src/components/mypage/MyLikedContainer.jsx
+++ src/components/mypage/MyLikedContainer.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 // redux
 import { useAppDispatch, useAppSelector } from '../../redux/store';
-import { setFilter, setFilterDetail } from '../../redux/mypageSlice';
 // style.js, component
 import * as S from './MyMenu.style';
 import MyFilterBar from './MyFilterBar';
