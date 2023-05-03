a1bd04c38a06dcb388efb56ea6e0af7e13993c0f
yyeonzu <tooma22@naver.com>
2023-05-03 13:05:51 +0900

    feat: 검색 api 연결

--- src/components/searchpage/Search.jsx
+++ src/components/searchpage/Search.jsx
@@ -5,7 +5,7 @@ import Footer from '../_common/footer/Footer';
 import searchicon from '../../assets/images/searchpage/searchicon.png';
 import { GetSearchBooth } from '../../api/booth';
 
-import { getbooths } from '../../api/_mock/boothmock';
+// import { getbooths } from '../../api/_mock/boothmock';
 
 const Search = () => {
   const [inputText, setInputText] = useState('');
@@ -14,20 +14,20 @@ const Search = () => {
 
   const onSubmit = () => {
     setKeyword("'" + inputText + "'");
-    GetSearchBooth(keyword)
+    GetSearchBooth(inputText)
       .then(res => {
-        console.log(res);
-        setBooth(getbooths);
+        setBooth(res.data.data);
       })
       .catch(err => console.log(err));
   };
+  console.log(booth);
   return (
     <>
       <S.Wrapper>
         <TopBar title='부스 정보 검색'></TopBar>
         <S.InputContainer>
           <S.Input
-            placeholder='부스를 검색하세요'
+            placeholder='부스나 메뉴 이름을 검색하세요'
             type='text'
             value={inputText}
             onChange={e => setInputText(e.target.value)}
@@ -42,7 +42,7 @@ const Search = () => {
 
         <S.TextContainer>
           <div>{keyword}에 대한 검색결과</div>
-          <div>총 2개의 부스</div>
+          <div>총 {booth.length ? booth.length : 0}개의 부스</div>
         </S.TextContainer>
       </S.Wrapper>
       <Footer />
