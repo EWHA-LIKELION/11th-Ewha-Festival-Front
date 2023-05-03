e749e7d6180635784de309bd8541d93780e19526
gchaewon <conjs1@naver.com>
2023-05-03 19:50:06 +0900

    chore: 불필요한 코드 삭제

--- src/components/authpage/LoginMenu.jsx
+++ src/components/authpage/LoginMenu.jsx
@@ -44,22 +44,14 @@ const LoginMenu = () => {
               isTF: res.data.data.is_tf,
             }),
           );
-          // 부스 유저인경우 부스 아이디 & 부스 이름 저장
+          // 부스 유저인경우 부스 아이디 저장
           if (res.data.data.is_booth) {
-            console.log(res.data);
             // test
             dispatch(
               setBooth_id({
                 booth_id: res.data.data.booth_id,
               }),
             );
-            GetBooth(res.data.data.booth_id).then(res => {
-              dispatch(
-                setBooth_name({
-                  booth_name: res.data.data.name,
-                }),
-              );
-            });
           }
         });
       })
