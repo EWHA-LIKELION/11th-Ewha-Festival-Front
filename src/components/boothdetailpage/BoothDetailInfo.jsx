c5107f3c4c58784c4cd725ed80f704c55efca4c5
서진 <dltjwls0529@gmail.com>
2023-05-06 21:49:07 +0900

    chore: env 수정 빌드

--- src/components/boothdetailpage/BoothDetailInfo.jsx
+++ src/components/boothdetailpage/BoothDetailInfo.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import PartTitle from './PartTitle';
 import ImageGallery from './ImageGallery';
 import { useMap } from './useMap';
