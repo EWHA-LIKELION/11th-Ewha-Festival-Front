7d2d0085b00dd523b3cc534d191d0f4ec6b106f8
yyeonzu <tooma22@naver.com>
2023-05-07 22:42:44 +0900

    chore: 목데이터 테스트

--- src/api/_mock/eventmock.js
+++ src/api/_mock/eventmock.js
@@ -1,20 +1,20 @@
 export const event1 = [
   { id: 1, title: '개막식', time: '11:00-12:00' },
-  { id: 2, title: '개막식', time: '11:00-12:00' },
-  { id: 3, title: '개막식', time: '11:00-12:00' },
-  { id: 4, title: '개막식', time: '11:00-12:00' },
+  { id: 2, title: '안우진', time: '11:00-12:00' },
+  { id: 3, title: '요키시', time: '11:00-12:00' },
+  { id: 4, title: '후라도', time: '11:00-12:00' },
 ];
 
 export const event2 = [
   { id: 1, title: '둘재날', time: '11:00-12:00' },
-  { id: 2, title: '개막식', time: '11:00-12:00' },
-  { id: 3, title: '개막식', time: '11:00-12:00' },
-  { id: 4, title: '개막식', time: '11:00-12:00' },
+  { id: 2, title: '최원태', time: '11:00-12:00' },
+  { id: 3, title: '장재영', time: '11:00-12:00' },
+  { id: 4, title: '정찬헌', time: '11:00-12:00' },
 ];
 
 export const event3 = [
   { id: 1, title: '마지막날', time: '11:00-12:00' },
-  { id: 2, title: '개막식', time: '11:00-12:00' },
-  { id: 3, title: '개막식', time: '11:00-12:00' },
-  { id: 4, title: '개막식', time: '11:00-12:00' },
+  { id: 2, title: '김재웅', time: '11:00-12:00' },
+  { id: 3, title: '김동혁', time: '11:00-12:00' },
+  { id: 4, title: '이승호', time: '11:00-12:00' },
 ];
